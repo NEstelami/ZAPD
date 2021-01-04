1429ea73b56f82dc3ac9a1a5e564bd2405b912ba
Kenix3 <kenixwhisperwind@gmail.com>
2021-01-04 18:08:57 -0500

    Bitconverter now supports more types and should correctly read floating points.

--- ZAPD/BitConverter.h
+++ ZAPD/BitConverter.h
@@ -6,38 +6,107 @@
 class BitConverter
 {
 public:
-	static inline int16_t ToInt16BE(uint8_t* data, int offset)
+	static inline int8_t ToInt8BE(const uint8_t* data, int offset)
 	{
-		return (data[offset + 0] << 8) + data[offset + 1];
+		return (uint8_t)data[offset + 0];
 	}
 
-	static inline int16_t ToInt16BE(std::vector<uint8_t> data, int offset)
+	static inline int8_t ToInt8BE(const std::vector<uint8_t>& data, int offset)
 	{
-		return (data[offset + 0] << 8) + data[offset + 1];
+		return (uint8_t)data[offset + 0];
 	}
 
-	static inline int32_t ToInt32BE(std::uint8_t* data, int offset)
+	static inline uint8_t ToUInt8BE(const uint8_t* data, int offset)
 	{
-		return (data[offset + 0] << 24) + (data[offset + 1] << 16) + (data[offset + 2] << 8) + data[offset + 3];
+		return (uint8_t)data[offset + 0];
 	}
 
-	static inline int32_t ToInt32BE(std::vector<uint8_t> data, int offset)
+	static inline uint8_t ToUInt8BE(const std::vector<uint8_t>& data, int offset)
 	{
-		return (data[offset + 0] << 24) + (data[offset + 1] << 16) + (data[offset + 2] << 8) + data[offset + 3];
+		return (uint8_t)data[offset + 0];
 	}
 
-	static inline uint64_t ToInt64BE(uint8_t* data, int offset)
+	static inline int16_t ToInt16BE(const uint8_t* data, int offset)
+	{
+		return ((uint16_t)data[offset + 0] << 8) + (uint16_t)data[offset + 1];
+	}
+
+	static inline int16_t ToInt16BE(const std::vector<uint8_t>& data, int offset)
+	{
+		return ((uint16_t)data[offset + 0] << 8) + (uint16_t)data[offset + 1];
+	}
+
+	static inline uint16_t ToUInt16BE(const uint8_t* data, int offset)
+	{
+		return ((uint16_t)data[offset + 0] << 8) + (uint16_t)data[offset + 1];
+	}
+
+	static inline uint16_t ToUInt16BE(const std::vector<uint8_t>& data, int offset)
+	{
+		return ((uint16_t)data[offset + 0] << 8) + (uint16_t)data[offset + 1];
+	}
+
+	static inline int32_t ToInt32BE(const uint8_t* data, int offset)
+	{
+		return ((uint32_t)data[offset + 0] << 24) + ((uint32_t)data[offset + 1] << 16) + ((uint32_t)data[offset + 2] << 8) + (uint32_t)data[offset + 3];
+	}
+
+	static inline int32_t ToInt32BE(const std::vector<uint8_t>& data, int offset)
+	{
+		return ((uint32_t)data[offset + 0] << 24) + ((uint32_t)data[offset + 1] << 16) + ((uint32_t)data[offset + 2] << 8) + (uint32_t)data[offset + 3];
+	}
+
+	static inline uint32_t ToUInt32BE(const uint8_t* data, int offset)
+	{
+		return ((uint32_t)data[offset + 0] << 24) + ((uint32_t)data[offset + 1] << 16) + ((uint32_t)data[offset + 2] << 8) + (uint32_t)data[offset + 3];
+	}
+
+	static inline uint32_t ToUInt32BE(const std::vector<uint8_t>& data, int offset)
+	{
+		return ((uint32_t)data[offset + 0] << 24) + ((uint32_t)data[offset + 1] << 16) + ((uint32_t)data[offset + 2] << 8) + (uint32_t)data[offset + 3];
+	}
+
+	static inline int64_t ToInt64BE(const uint8_t* data, int offset)
 	{
 		return ((uint64_t)data[offset + 0] << 56) + ((uint64_t)data[offset + 1] << 48) + ((uint64_t)data[offset + 2] << 40) + ((uint64_t)data[offset + 3] << 32) + ((uint64_t)data[offset + 4] << 24) + ((uint64_t)data[offset + 5] << 16) + ((uint64_t)data[offset + 6] << 8) + ((uint64_t)data[offset + 7]);
 	}
 
-	static inline uint64_t ToInt64BE(std::vector<uint8_t> data, int offset)
+	static inline int64_t ToInt64BE(const std::vector<uint8_t>& data, int offset)
 	{
 		return ((uint64_t)data[offset + 0] << 56) + ((uint64_t)data[offset + 1] << 48) + ((uint64_t)data[offset + 2] << 40) + ((uint64_t)data[offset + 3] << 32) + ((uint64_t)data[offset + 4] << 24) + ((uint64_t)data[offset + 5] << 16) + ((uint64_t)data[offset + 6] << 8) + ((uint64_t)data[offset + 7]);
 	}
 
-	static inline float ToFloatBE(std::vector<uint8_t> data, int offset)
+	static inline uint64_t ToUInt64BE(const uint8_t* data, int offset)
 	{
-		return (float)(data[offset + 0] << 24) + (data[offset + 1] << 16) + (data[offset + 2] << 8) + data[offset + 3];
+		return ((uint64_t)data[offset + 0] << 56) + ((uint64_t)data[offset + 1] << 48) + ((uint64_t)data[offset + 2] << 40) + ((uint64_t)data[offset + 3] << 32) + ((uint64_t)data[offset + 4] << 24) + ((uint64_t)data[offset + 5] << 16) + ((uint64_t)data[offset + 6] << 8) + ((uint64_t)data[offset + 7]);
+	}
+
+	static inline uint64_t ToUInt64BE(const std::vector<uint8_t>& data, int offset)
+	{
+		return ((uint64_t)data[offset + 0] << 56) + ((uint64_t)data[offset + 1] << 48) + ((uint64_t)data[offset + 2] << 40) + ((uint64_t)data[offset + 3] << 32) + ((uint64_t)data[offset + 4] << 24) + ((uint64_t)data[offset + 5] << 16) + ((uint64_t)data[offset + 6] << 8) + ((uint64_t)data[offset + 7]);
+	}
+
+	static inline float ToFloatBE(const uint8_t* data, int offset)
+	{
+		uint32_t floatData = ((uint32_t)data[offset + 0] << 24) + ((uint32_t)data[offset + 1] << 16) + ((uint32_t)data[offset + 2] << 8) + (uint32_t)data[offset + 3];
+		return *(float*)(&floatData);
+	}
+
+	static inline float ToFloatBE(const std::vector<uint8_t>& data, int offset)
+	{
+		uint32_t floatData = ((uint32_t)data[offset + 0] << 24) + ((uint32_t)data[offset + 1] << 16) + ((uint32_t)data[offset + 2] << 8) + (uint32_t)data[offset + 3];
+		return *(float*)(&floatData);
+	}
+
+	static inline double ToDoubleBE(const uint8_t* data, int offset)
+	{
+		uint64_t floatData = ((uint64_t)data[offset + 0] << 56) + ((uint64_t)data[offset + 1] << 48) + ((uint64_t)data[offset + 2] << 40) + ((uint64_t)data[offset + 3] << 32) + ((uint64_t)data[offset + 4] << 24) + ((uint64_t)data[offset + 5] << 16) + ((uint64_t)data[offset + 6] << 8) + ((uint64_t)data[offset + 7]);
+		return *(double*)(&floatData);
+	}
+
+	static inline double ToDoubleBE(const std::vector<uint8_t>& data, int offset)
+	{
+		uint64_t floatData = ((uint64_t)data[offset + 0] << 56) + ((uint64_t)data[offset + 1] << 48) + ((uint64_t)data[offset + 2] << 40) + ((uint64_t)data[offset + 3] << 32) + ((uint64_t)data[offset + 4] << 24) + ((uint64_t)data[offset + 5] << 16) + ((uint64_t)data[offset + 6] << 8) + ((uint64_t)data[offset + 7]);
+		return *(double*)(&floatData);
 	}
 };
