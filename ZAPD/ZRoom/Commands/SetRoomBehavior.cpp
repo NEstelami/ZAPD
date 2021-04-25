501fc69ff5ff37b15598bb87e1681b50b9cc4b1b
angie <angheloalf95@gmail.com>
2021-04-25 12:33:32 -0400

    Add again different commands for each game ni SCENE_CMD_ROOM_BEHAVIOR

--- ZAPD/ZRoom/Commands/SetRoomBehavior.cpp
+++ ZAPD/ZRoom/Commands/SetRoomBehavior.cpp
@@ -7,20 +7,33 @@ SetRoomBehavior::SetRoomBehavior(ZRoom* nZRoom, const std::vector<uint8_t>& rawD
                                  uint32_t rawDataIndex)
 	: ZRoomCommand(nZRoom, rawData, rawDataIndex)
 {
-	gameplayFlags = rawData.at(rawDataIndex + 0x01);
+}
+
+void SetRoomBehavior::ParseRawData()
+{
+	gameplayFlags = cmdArg1;
 	gameplayFlags2 = BitConverter::ToInt32BE(rawData, rawDataIndex + 0x04);
+
+	currRoomUnk2 = gameplayFlags2 & 0xFF;
+
+	currRoomUnk5 = showInvisActors = (gameplayFlags2 >> 8) & 1;
+
+	msgCtxUnk = (gameplayFlags2 >> 10) & 1;
+
+	enablePosLights = (gameplayFlags2 >> 11) & 1;
+	kankyoContextUnkE2 = (gameplayFlags2 >> 12) & 1;
 }
 
 std::string SetRoomBehavior::GetBodySourceCode() const
 {
-	uint8_t currRoomUnk2 = gameplayFlags2 & 0xFF;
-	uint8_t currRoomUnk5 = (gameplayFlags2 >> 8) & 1;
-	uint8_t msgCtxunk12044 = (gameplayFlags2 >> 10) & 1;
-	uint8_t enablePosLights = (gameplayFlags2 >> 11) & 1;
-	uint8_t kankyoContextUnkE2 = (gameplayFlags2 >> 12) & 1;
-	return StringHelper::Sprintf("SCENE_CMD_ROOM_BEHAVIOR(0x%02X, 0x%02X, %i, %i, %i, %i)",
-	                             gameplayFlags, currRoomUnk2, currRoomUnk5, msgCtxunk12044,
-	                             enablePosLights, kankyoContextUnkE2);
+	if (Globals::Instance->game == ZGame::MM_RETAIL)
+	{
+		return StringHelper::Sprintf("SCENE_CMD_ROOM_BEHAVIOR(0x%02X, 0x%02X, %i, %i, %i, %i)",
+		                             gameplayFlags, currRoomUnk2, currRoomUnk5, msgCtxUnk,
+		                             enablePosLights, kankyoContextUnkE2);
+	}
+	return StringHelper::Sprintf("SCENE_CMD_ROOM_BEHAVIOR(0x%02X, 0x%02X, %i, %i)", gameplayFlags,
+	                             currRoomUnk2, showInvisActors, msgCtxUnk);
 }
 
 std::string SetRoomBehavior::GetCommandCName() const
--- ZAPD/ZRoom/Commands/SetRoomBehavior.h
+++ ZAPD/ZRoom/Commands/SetRoomBehavior.h
@@ -7,12 +7,24 @@ class SetRoomBehavior : public ZRoomCommand
 public:
 	SetRoomBehavior(ZRoom* nZRoom, const std::vector<uint8_t>& rawData, uint32_t rawDataIndex);
 
+	void ParseRawData() override;
+
 	std::string GetBodySourceCode() const override;
 
 	RoomCommand GetRoomCommand() const override;
 	std::string GetCommandCName() const override;
 
-private:
+protected:
 	uint8_t gameplayFlags;
 	uint32_t gameplayFlags2;
+
+	uint8_t currRoomUnk2;
+
+	uint8_t showInvisActors;
+	uint8_t currRoomUnk5;
+
+	uint8_t msgCtxUnk;
+
+	uint8_t enablePosLights;
+	uint8_t kankyoContextUnkE2;
 };
