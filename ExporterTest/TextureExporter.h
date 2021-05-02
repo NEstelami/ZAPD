5531ad481f81019a5dfbfaff40b6c5fad207223e
Nicholas Estelami <NEstelami@users.noreply.github.com>
2021-05-02 07:48:49 -0400

    minor cleanup

--- ExporterTest/TextureExporter.h
+++ ExporterTest/TextureExporter.h
@@ -7,8 +7,5 @@
 class ExporterExample_Texture : public ZResourceExporter
 {
 public:
-	friend class ZTexture;
-
-	// Inherited via ZResourceExporter
 	virtual void Save(ZResource* res, std::string outPath, BinaryWriter* writer) override;
 };
