50343256a0fd8f67df8224d53083d06bb3d0763f
Nicholas Estelami <NEstelami@users.noreply.github.com>
2021-05-05 20:47:35 -0400

    Minor commenting

--- ExporterTest/Main.cpp
+++ ExporterTest/Main.cpp
@@ -7,9 +7,12 @@
 
 void ImportExporters()
 {
+	// In this example we set up a new exporter called "EXAMPLE".
+	// By running ZAPD with the argument -sexp EXAMPLE, we tell it that we want to use this exporter for our resources.
 	Globals::AddExporter("EXAMPLE", ZResourceType::Texture, new ExporterExample_Texture());
 	Globals::AddExporter("EXAMPLE", ZResourceType::Room, new ExporterExample_Room());
 	Globals::AddExporter("EXAMPLE", ZResourceType::CollisionHeader, new ExporterExample_Collision());
 }
 
+// When ZAPD starts up, it will automatically call the below function, which in turn sets up our exporters.
 REGISTER_EXPORTER(ImportExporters)
