4f2d3815d445ded6f89e38771960c997daa57c67
angie <angheloalf95@gmail.com>
2021-05-11 14:11:03 -0400

    Add SCENE_CMD_UNK_09 todo

--- ZAPD/ZRoom/Commands/Unused09.cpp
+++ ZAPD/ZRoom/Commands/Unused09.cpp
@@ -7,6 +7,8 @@ Unused09::Unused09(ZFile* nParent) : ZRoomCommand(nParent)
 
 std::string Unused09::GetBodySourceCode() const
 {
+	// TODO: uncomment when both repos have updated their macros
+	//return "SCENE_CMD_UNK_09()";
 	return StringHelper::Sprintf("{ %s, 0x00, 0x00 }", GetCommandHex().c_str());
 }
 
