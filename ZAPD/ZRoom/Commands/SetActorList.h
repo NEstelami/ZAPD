66b91d8bff78c699cf6c7dd881ccfdc9b7dd18f2
angie <angheloalf95@gmail.com>
2021-06-02 23:46:37 -0400

    Clean up actorlist output

--- ZAPD/ZRoom/Commands/SetActorList.h
+++ ZAPD/ZRoom/Commands/SetActorList.h
@@ -13,16 +13,18 @@ public:
 	int32_t GetRawDataSize() const;
 
 	uint16_t GetActorId() const;
+	void SetLargestActorName(size_t nameSize);
 
 protected:
 	uint16_t actorNum;
 	int16_t posX;
 	int16_t posY;
 	int16_t posZ;
-	int16_t rotX;
-	int16_t rotY;
-	int16_t rotZ;
+	uint16_t rotX;
+	uint16_t rotY;
+	uint16_t rotZ;
 	uint16_t initVar;
+	size_t largestActorName = 30;
 };
 
 class SetActorList : public ZRoomCommand
