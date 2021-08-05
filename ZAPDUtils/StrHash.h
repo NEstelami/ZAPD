f2d6a52691eaff89ade40d8477406c41170390ea
Nicholas Estelami <NEstelami@users.noreply.github.com>
2021-08-05 11:01:42 -0400

    Minor fix

--- ZAPDUtils/StrHash.h
+++ ZAPDUtils/StrHash.h
@@ -28,7 +28,7 @@ static strhash CRC32B(unsigned char* message, int32_t size)
 	return ~(uint32_t)(crc);
 }
 
-constexpr static strhash CRC32BCT(unsigned char* message, int32_t size)
+constexpr static strhash CRC32BCT(const char* message, int32_t size)
 {
 	int32_t byte = 0, crc = 0;
 	int32_t mask = 0;
