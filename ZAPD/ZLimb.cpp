259a778096c1b6415d0357b7c2f83a613af32874
Random <28494085+Random06457@users.noreply.github.com>
2021-08-14 18:38:46 +0200

    remove useless comment

--- ZAPD/ZLimb.cpp
+++ ZAPD/ZLimb.cpp
@@ -447,7 +447,6 @@ void ZLimb::ParseRawData()
 	case ZLimbType::LOD:
 		dList2Ptr = BitConverter::ToUInt32BE(rawData, rawDataIndex + 12);
 		[[fallthrough]];
-		// Intended fallthrough
 	case ZLimbType::Standard:
 		dListPtr = BitConverter::ToUInt32BE(rawData, rawDataIndex + 8);
 		break;
