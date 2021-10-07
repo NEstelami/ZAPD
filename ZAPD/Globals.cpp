ac71a66a31c2209e9ce846b077d90759482fceb6
Louis <louist103@gmail.com>
2021-10-07 16:36:06 -0400

    Fix warning (error)

--- ZAPD/Globals.cpp
+++ ZAPD/Globals.cpp
@@ -25,7 +25,6 @@ Globals::Globals()
 	useExternalResources = true;
 	lastScene = nullptr;
 	verbosity = VerbosityLevel::VERBOSITY_SILENT;
-	currentExporter;
 	outputPath = Directory::GetCurrentDirectory();
 }
 
