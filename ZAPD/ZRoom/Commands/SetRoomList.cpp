14d2a734b3fdddf2091629edc59db94ab5548f3e
louist103 <35883445+louist103@users.noreply.github.com>
2022-01-20 22:52:49 -0500

    Remove cast for scene file lists (#219)
    * use uintptr_t for scenes
    
    * Update ZAPD/ZRoom/Commands/SetRoomList.cpp
    
    Co-authored-by: Anghelo Carvajal <angheloalf95@gmail.com>
    
    * Update Jenkinsfile
    
    Co-authored-by: Louis <louist103@pop-os.localdomain>
    Co-authored-by: Anghelo Carvajal <angheloalf95@gmail.com>

--- ZAPD/ZRoom/Commands/SetRoomList.cpp
+++ ZAPD/ZRoom/Commands/SetRoomList.cpp
@@ -105,7 +105,7 @@ std::string RomFile::GetBodySourceCode() const
 					declaration += "\n";
 
 				declaration +=
-					StringHelper::Sprintf("\t{ (u32)_%sSegmentRomStart, (u32)_%sSegmentRomEnd },",
+					StringHelper::Sprintf("\t{ _%sSegmentRomStart, _%sSegmentRomEnd },",
 				                          roomName.c_str(), roomName.c_str());
 				isFirst = false;
 			}
