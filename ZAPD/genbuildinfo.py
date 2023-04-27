6a2f3cb9b49a178ef3fa25eb4665029e9be3d1cf
Nicholas Estelami <NEstelami@users.noreply.github.com>
2023-04-27 00:40:31 -0400

    Fixed build error from missing .git directory

--- ZAPD/genbuildinfo.py
+++ ZAPD/genbuildinfo.py
@@ -4,13 +4,22 @@ import argparse
 from datetime import datetime
 import getpass
 import subprocess
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--devel", action="store_true")
 args = parser.parse_args()
 
 with open("build/ZAPD/BuildInfo.cpp", "w+") as buildFile:
-    label = subprocess.check_output(["git", "describe", "--always"]).strip().decode("utf-8")
+    # Before calling git confirm that a .git directory exists.
+    # If it does not exist, a default label will be used instead.
+
+    
+    if (os.path.isdir(".git")):
+        label = subprocess.check_output(["git", "describe", "--always"]).strip().decode("utf-8")
+    else:
+        label = "GIT_NOT_FOUND"
+
     now = datetime.now()
     if args.devel:
         label += " ~ Development version"
