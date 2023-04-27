588cb6d76b6405dfeb7a12c09a2685ccce0da3ae
Nicholas Estelami <NEstelami@users.noreply.github.com>
2023-04-27 17:59:13 -0400

    Improved git failsafe

--- ZAPD/genbuildinfo.py
+++ ZAPD/genbuildinfo.py
@@ -4,20 +4,17 @@ import argparse
 from datetime import datetime
 import getpass
 import subprocess
-import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--devel", action="store_true")
 args = parser.parse_args()
 
 with open("build/ZAPD/BuildInfo.cpp", "w+") as buildFile:
-    # Before calling git confirm that a .git directory exists.
-    # If it does not exist, a default label will be used instead.
-
-    
-    if (os.path.isdir(".git")):
+    # Get commit hash from git
+    # If git fails due to a missing .git directory, a default label will be used instead.
+    try:
         label = subprocess.check_output(["git", "describe", "--always"]).strip().decode("utf-8")
-    else:
+    except:
         label = "GIT_NOT_FOUND"
 
     now = datetime.now()
